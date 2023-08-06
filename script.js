d2a2f1af686f80e5a75c1efb6ed7aa4711a768ab
mga-time-studio <123512807+mga-time-studio@users.noreply.github.com>
2023-08-06 14:43:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block"; // Показываем всплывающее окно
+  }
+  
+  function hidePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none"; // Скрываем всплывающее окно
+  }
+  
